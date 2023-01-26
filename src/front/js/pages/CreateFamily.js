d5e2ab7768f27a0fb69bee4d5d0b39d4aa14ed54
pablotori98 <pablotori98@gmail.com>
2023-01-26 14:59:35 +0000

    Creador de familias funcional

--- src/front/js/pages/CreateFamily.js
+++ src/front/js/pages/CreateFamily.js
@@ -1,19 +1,22 @@
 //Import react
 import React, { useEffect, useState } from "react";
 //Import material
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, InputAdornment, TextField, Typography } from "@mui/material";
+
+import tree from "../../img/treee.jpg"
 
 export const CreateFamily = ()=>{
     const token = sessionStorage.getItem("access_token")
     const current_user= sessionStorage.getItem("current_user")
     const [apellido, setApellido] = useState("")
-    useEffect(() => {
+   
+    const funcionllamada = async () =>{
         var myHeaders = new Headers();
-        myHeaders.append("Authorization", token);
+        myHeaders.append("Authorization",  `Bearer ${sessionStorage.getItem("access_token")}`);
         myHeaders.append("Content-Type", "application/json");
         
         var raw = JSON.stringify({
-          "last_name": {apellido}
+          "last_name": apellido
         });
         
         var requestOptions = {
@@ -22,25 +25,26 @@ export const CreateFamily = ()=>{
           body: raw,
           redirect: 'follow'
         };
-        
-        fetch(`${process.env.BACKEND_URL}/api/createfamily/${current_user}`, requestOptions)
+        await fetch(`${process.env.BACKEND_URL}/api/createfamily/${current_user}`, requestOptions)
           .then(response => response.text())
           .then(result => console.log(result))
           .catch(error => console.log('error', error));
-      }, []);
-    
+    }
 
-
+    console.log(token)
     return (
         <>
         {token?    
-        <Box className="d-flex flex-column align-items-center">
-            <Typography>Creación de familias</Typography>
-            <TextField className="my-2" label='Apellido' onChange={(event)=> setApellido(event.target.value)}/>
-            <TextField className="my-2"/>
-            <TextField className="my-2"/>
-            <TextField className="my-2"/>
-            <Button>Crear Familia</Button>
+        <Box className="d-flex flex-column align-items-center mt-5 height100">
+            <Typography variant="h2" className="mt-5">Creación de familias</Typography>
+            <TextField className="my-2 text-center my-3 w-25" label="Apellido" name="apellido" onChange={(event)=> setApellido(event.target.value)}
+            inputProps={{min: 0, style: { textAlign: 'center' }}}
+            />
+
+
+            <Button className="glow-on-hover text-white" onClick={funcionllamada}><strong>Crear Familia</strong></Button>
+
+            <img className="mt-3" src={tree}/>
         </Box>
         :
         
